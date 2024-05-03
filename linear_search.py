70190bdb56f9d7261d027a01c2e1d1468a78449b
Julio Gaekel <jriveragaekel@gmail.com>
2024-05-03 17:09:25 +0200

    Add verify function and tests

--- linear_search.py
+++ linear_search.py
@@ -8,4 +8,16 @@ def linear_search(list, target):
     return None
 
 
-def 
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
+verify(result)
